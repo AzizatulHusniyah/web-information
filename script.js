15dbc26d9017ff9c3a8fd42f35b2aece02cf811a
AzizatulHusniyah <149143493+AzizatulHusniyah@users.noreply.github.com>
2024-10-21 21:41:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,3 @@
 window.onload = function() {
     alert('Selamat datang di situs IMAGE UNESA!');
 };
-
-// Tambahkan scroll halus ketika menavigasi ke bagian lain di halaman
-document.querySelectorAll('nav a').forEach(function(link) {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        document.getElementById(targetId).scrollIntoView({
-            behavior: 'instant'
-        });
-    });
-});
